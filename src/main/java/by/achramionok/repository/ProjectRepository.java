4f83004bdb151540cfb81c5ee25e3b753cf0c32c
Kirill Achramionok <mtanl8957@gmail.com>
2017-04-03 12:06:24 +0300

    Update Repository

--- src/main/java/by/achramionok/repository/ProjectRepository.java
+++ src/main/java/by/achramionok/repository/ProjectRepository.java
@@ -3,11 +3,13 @@ package by.achramionok.repository;
 import by.achramionok.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.Set;
+
 /**
  * Created by Kirill on 22.03.2017.
  */
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
     Project findById(Integer id);
     Project findByName(String name);
-
 }
--- src/main/java/by/achramionok/repository/UserRepository.java
+++ src/main/java/by/achramionok/repository/UserRepository.java
@@ -2,7 +2,12 @@ package by.achramionok.repository;
 
 import by.achramionok.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 /**
  * Created by Kirill on 21.03.2017.
@@ -11,6 +16,9 @@ public interface UserRepository extends JpaRepository<User, Integer>{
     User findById(Integer idUser);
     User findByEmail(String email);
     User findByUserName(String UserName);
-
+    Set<User> findAllByRoleAndProjectsId(Integer role,Integer id);
+    //Set<User> findAllByProjectsIdNotAndRole(Integer id,Integer role);
+//    @Query("SELECT u.id, u.userName, u.role FROM User u CROSS JOIN Project p ON p.id <> :id and u.role = :role")
+//    Set<User> findFreeUsers(@Param("id") Integer id, @Param("role") Integer role);
     void delete(Integer idUser);
 }
